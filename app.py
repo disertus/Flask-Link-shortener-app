dacb3ab9bc99f6ecc3b85a197d3c39900bbda41c
Roman Zahorskyi <zagorsky.roman@gmail.com>
2020-11-29 21:43:37 +0200

    Added the possibility to add post requests and made the template react to changes.

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template  #allows to return html templates instead of manually generated layouts
+from flask import (
+    Flask,
+    render_template,
+    request,
+)  # allows to return html templates instead of manually generated layouts
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return render_template('home.html', name='Roman') #passing a variable to the html placeholder, allows to present dynamic content for different users
+    # passing a variable to the html placeholder, allows to present dynamic content for different users
+    return render_template("home.html", name="by Roman")
 
-@app.route('/hi')
+
+@app.route("/hi")
 def hi_there():
-    return 'Hi there, pretty!'
+    return "Hi there, pretty!"
 
 
-if __name__ == '__main__':
+@app.route(
+    "/your-url", methods=['GET', 'POST']
+)  # the action after submitting the form in home.html leads to your-url
+def your_url():
+    if request.method == 'POST':
+        # args = dictionary for parameters that can be passed. here it represents the value passed to 'code' inside the home.html form
+        return render_template("your_url.html", code=request.form["code"]) #.form is used with POST requests
+    else:
+        return 'This is not valid'
+
+
+if __name__ == "__main__":
     app.run(debug=1)
